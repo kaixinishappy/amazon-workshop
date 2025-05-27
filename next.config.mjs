eeb78a35f98aa84c87f2e236f16d191792cbd87d
Kai Xin <kxt24@ic.ac.uk>
2025-05-27 12:37:37 +0100

    Frontend part

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/pokemon',
+                destination: 'https://qkxkh220o3.execute-api.eu-west-2.amazonaws.com/Prod/pokemon',
+            },
+            {
+                source: '/api/pokemon/:id',
+                destination: 'https://qkxkh220o3.execute-api.eu-west-2.amazonaws.com/Prod/pokemon/:id',
+            },
+        ];
+    }
+ };
+ 
+ 
+ export default nextConfig;
---
+++ src/model/pokemon.ts
@@ -0,0 +1,14 @@
+export default interface Pokemon {
+    pokemonNumber: number;
+    attack: number;
+    defense: number;
+    devolution: string;
+    evolution: string;
+    evolutionFamily: string[];
+    healthPoints: number;
+    mainImage: string;
+    pokemonName: string;
+    pokemonType: string[];
+    speed: number;
+  }
+  
